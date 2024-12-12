1c0ce8c2d83eb569240662486bdf34ec55f17f8e
BobbyLin23 <linzhangsheng23@gmail.com>
2024-12-12 22:41:45 +0800

    feat: add toolbar

--- components/editor/TheEditor.vue
+++ components/editor/TheEditor.vue
@@ -6,6 +6,7 @@ import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
+import Underline from '@tiptap/extension-underline'
 import { StarterKit } from '@tiptap/starter-kit'
 import { Editor, EditorContent } from '@tiptap/vue-3'
 import ImageResize from 'tiptap-extension-resize-image'
@@ -61,6 +62,7 @@ onMounted(() => {
       TableCell,
       Image,
       ImageResize,
+      Underline,
     ],
   })
 })
--- components/editor/Toolbar.vue
+++ components/editor/Toolbar.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
-import { type LucideIcon, Undo2 } from 'lucide-vue-next'
+import {
+  Bold,
+  Italic,
+  ListTodo,
+  type LucideIcon,
+  MessageSquarePlus,
+  Printer,
+  Redo2,
+  RemoveFormatting,
+  SpellCheck,
+  Underline,
+  Undo2,
+} from 'lucide-vue-next'
 import { useEditorStore } from '~/store/editor'
 
 const { editor } = useEditorStore()
@@ -16,6 +28,64 @@ const sections: {
       icon: Undo2,
       onClick: () => editor?.chain().focus().undo().run(),
     },
+    {
+      label: 'Redo',
+      icon: Redo2,
+      onClick: () => editor?.chain().focus().redo().run(),
+    },
+    {
+      label: 'Print',
+      icon: Printer,
+      onClick: () => window.print(),
+    },
+    {
+      label: 'Spell Check',
+      icon: SpellCheck,
+      onClick: () => {
+        const current = editor?.view.dom.getAttribute('spellcheck')
+        editor?.view.dom.setAttribute('spellcheck', current === 'false' ? 'true' : 'false')
+      },
+    },
+  ],
+  [
+    {
+      label: 'Bold',
+      icon: Bold,
+      onClick: () => editor?.chain().focus().toggleBold().run(),
+      isActive: editor?.isActive('bold'),
+    },
+    {
+      label: 'Italic',
+      icon: Italic,
+      onClick: () => editor?.chain().focus().toggleItalic().run(),
+      isActive: editor?.isActive('italic'),
+    },
+    {
+      label: 'Underline',
+      icon: Underline,
+      onClick: () => editor?.chain().focus().toggleUnderline().run(),
+      isActive: editor?.isActive('underline'),
+    },
+  ],
+  [
+    {
+      label: 'Comment',
+      icon: MessageSquarePlus,
+      onClick: () => {},
+      isActive: false,
+    },
+    {
+      label: 'List Todo',
+      icon: ListTodo,
+      onClick: () => editor?.chain().focus().toggleTaskList().run(),
+      isActive: editor?.isActive('taskList'),
+    },
+    {
+      label: 'Remove Formatting',
+      icon: RemoveFormatting,
+      onClick: () => editor?.chain().focus().clearNodes().run(),
+      isActive: false,
+    },
   ],
 ]
 </script>
@@ -23,5 +93,15 @@ const sections: {
 <template>
   <div class="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
     <ToolbarButton v-for="item in sections[0]" :key="item.label" :icon="item.icon" @click="item.onClick" />
+    <Separator orientation="vertical" class="h-6 bg-neutral-300" />
+
+    <Separator orientation="vertical" class="h-6 bg-neutral-300" />
+
+    <Separator orientation="vertical" class="h-6 bg-neutral-300" />
+
+    <Separator orientation="vertical" class="h-6 bg-neutral-300" />
+    <ToolbarButton v-for="item in sections[1]" :key="item.label" :icon="item.icon" :is-active="item.isActive" @click="item.onClick" />
+    <Separator orientation="vertical" class="h-6 bg-neutral-300" />
+    <ToolbarButton v-for="item in sections[2]" :key="item.label" :icon="item.icon" :is-active="item.isActive" @click="item.onClick" />
   </div>
 </template>
